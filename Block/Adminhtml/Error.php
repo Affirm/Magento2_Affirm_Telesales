6f6d68a3e6f2c0a83e1f57d06604c33dd764263a
taehyunlim <taehyun.lim@gmail.com>
2021-03-01 20:12:58 -0800

    replace placeholder link

--- Block/Adminhtml/Error.php
+++ Block/Adminhtml/Error.php
@@ -7,7 +7,7 @@ class Error extends \Magento\Framework\View\Element\Template
     /**
      * Fixed url for Affirm Telesales Documentation.
      */
-    const TELESALES_DOC_URL = "https://www.affirm.com/docs";
+    const TELESALES_DOC_URL = "https://www.affirm.com/docs"; // Todo: Update the link once available
 
     /**
      * Retrieve Affirm Telesales Doc URL
